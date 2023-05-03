fcb6c61bed34bad1685cc92230a4a8aedb4cb872
Mancabm <mancabmor1@alum.us.es>
2023-05-03 19:41:44 +0200

    Inicialización

---
+++ ExampleForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_Mongo
+{
+    public partial class ExampleForm : Form
+    {
+        public ExampleForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            printLabel.Text = "Hello World";
+        }
+    }
+}
---
+++ ProgramMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C_Mongo;
+
+namespace C_Mongo
+{
+    class ProgramMain
+    {
+        public static void Main(string[] args)
+        {
+            ExampleForm form = new ExampleForm();
+            form.ShowDialog();
+        }
+    }
+}
